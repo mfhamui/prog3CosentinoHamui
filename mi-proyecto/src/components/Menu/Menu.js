0ec5c547490a2104421b2dade1eafa4f9dc0d386
mfhamui <mfhamui@gmail.com>
2025-09-13 15:24:10 -0300

    cambios en el css del menu

--- mi-proyecto/src/components/Menu/Menu.js
+++ mi-proyecto/src/components/Menu/Menu.js
@@ -8,9 +8,13 @@ function Menu(props) {
 const items = props.itemsMenu 
   return (
     <React.Fragment>
-    <h1>TV Fan </h1>
+    <h1 className= "titulo">TV Fan </h1>
     <nav className="nav">
-      
+      <ul className="user">
+        <li>
+          <img src="/assets/img/logo.png" alt="" />
+        </li>
+      </ul>
       <ul className="menu">
         {items.map((item, idx) => (
           <ElementoMenu
